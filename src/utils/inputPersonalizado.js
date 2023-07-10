b0cc40f04498d704d526d3fa4e0cb2fa5a07c7a4
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2023-07-10 12:03:55 -0300

    fix

--- src/utils/inputPersonalizado.js
+++ src/utils/inputPersonalizado.js
@@ -2,7 +2,7 @@ import { InputGroup,Input,InputLeftAddon } from "@chakra-ui/react"
 export default function InputPersonalizado ({type,label,w,onChange}){
  return (
     <InputGroup w={w}>
-                <InputLeftAddon children={label} />
+                <InputLeftAddon>{label}</InputLeftAddon>
                 <Input variant="filled" type={type} onChange={onChange} />
     </InputGroup>
  )
