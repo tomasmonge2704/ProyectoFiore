d184a692b7621a989fa1725fa257a8de07eb4567
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2023-07-24 15:01:53 -0300

    fixes

--- src/components/context/carterasContext.js
+++ src/components/context/carterasContext.js
@@ -7,7 +7,7 @@ export const CarteraProveedoresContext = createContext();
 export function CarterasProvider({ children }) {
   const [CarteraBancaria, setCarteraBancaria] = useState([
     { empresa: "DPL Trading LLC",direccion:"30 North Gould Street, Suit R",direccion2:"Sheridan, WY 82801, USA", vatNumber:"92-0566625"},
-    { empresa: "Duplo",cuit:"23124342423",direccion:"asfgsdg", vatNumber:52342 },
+    { empresa: "Duplo",direccion:"40 North Gould Street, Suit R",direccion2:"Sheridan, WY 8000, USA", vatNumber:"90-000000"},
   ]);
   const [CarteraClientes, setCarteraClientes] = useState([
     { cliente: "Test 1",nombre:"test 1",direccion:"malagrino",codigoPostal:"1636",pais:"arg",cuit:"2141241" },
@@ -15,7 +15,7 @@ export function CarterasProvider({ children }) {
   ]);
   const [CarteraProveedores, setProveedores] = useState([
     { empresa: "REFINERIA DEL CENTRO S.A.",direccion:"Cno. a Jesús María KM. 10 1/2",direccion2:"(5145) Juárez Celman",pais:"Prov. de Córdoba - Rep. Argentina",cuit:"33-50134847-9"},
-    { empresa: "TEST S.A.",direccion:"Cno. a Jesús María KM. 10 1/2",direccion2:"(5145) Juárez Celman",pais:"Prov. de Córdoba - Rep. Argentina",cuit:"33-50134847-9"},
+    { empresa: "TEST S.A.",direccion:"Test Jesús María KM. 10 1/2",direccion2:"(5145) Juárez Celman",pais:"Test - Rep. Argentina",cuit:"44-50134847-9"},
   ]);
   return (
     <CarteraBancariaContext.Provider
