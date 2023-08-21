cda6301de593aa27fdb2c9d8c2fb94e161284110
tomasmonge2704 <tomas.monge2704@gmail.com>
2023-08-21 16:20:15 -0300

    modifique el componente table

--- src/pages/cartera-proveedores/index.js
+++ src/pages/cartera-proveedores/index.js
@@ -1,13 +1,18 @@
 import { TablePagination } from "@/utils/tablePagination";
-import { DeleteIcon } from "@chakra-ui/icons";
-import { FiSave } from "react-icons/fi";
-import { Tr, Td, IconButton, Input, Tbody,Center,Spinner,useToast} from "@chakra-ui/react";
-import { useEffect,useState } from "react";
+import {Center,Spinner} from "@chakra-ui/react";
+import { useState,useEffect } from "react";
 export default function Proveedores () {
-  const params = ["Company Name", "Address Line 1", "Address Line 2", "Country","Tax ID","Plant No.","Brand","ACTIONS"];
+  const params = [
+    {label:"Company Name",param:"nombre"},
+    {label:"Address Line 1",param:"direccion"},
+    {label:"Address Line 2",param:"direccion2"},
+    {label:"Country",param:"country"},
+    {label:"Tax ID",param:"taxId"},
+    {label:"Plant No.",param:"plantNumber"},
+    {label:"Brand",param:"brand"},
+  ];
   const [CarteraProveedores, setCarteraProveedores] = useState(undefined);
   const [loadData, setLoadData] = useState(false);
-  const toast = useToast();
   useEffect(() => {
     const token = localStorage.getItem("token");
     fetch(`${process.env.API_URL}/proveedor`, {
@@ -21,168 +26,13 @@ export default function Proveedores () {
         setLoadData(true);
       });
   }, []);
-  const Estructura = (itemsToDisplay, setItemsToDisplay) => {
-    const token = localStorage.getItem("token");
-    const handleChangeInput = (event, id, parameter) => {
-      const updatedProductos = itemsToDisplay.map((producto) => {
-        if (producto._id === id) {
-          return {
-            ...producto,
-            modified: true,
-            [parameter]: event.target.value,
-          };
-        }
-        return producto;
-      });
-      setItemsToDisplay(updatedProductos);
-    };
-    const handleDelete = (id) => { 
-      fetch(`${process.env.API_URL}/proveedor/${id}`, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        }})
-      .then((response) => response.json())
-      .then((data) => {
-        toast({
-          title: "Cliente",
-          description: `Se ha borrado correctamente.`,
-          status: "success",
-          position: "top-right",
-          duration: 5000,
-          isClosable: true,
-        });
-      })
-      .finally(() => {
-        const filtered = itemsToDisplay.filter((e) => e._id !== id);
-        setItemsToDisplay(filtered)
-      });
-    };
-    const handleUpdate = (id) => {
-      const buscado = itemsToDisplay.find((element) => element._id === id);
-      fetch(`${process.env.API_URL}/proveedor/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(buscado)
-      })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error en la solicitud");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        toast({
-          title: "Cliente",
-          description: `Se ha guardado correctamente ${data.nombre}.`,
-          status: "success",
-          position: "top-right",
-          duration: 5000,
-          isClosable: true,
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        toast({
-          title: "Error",
-          description: "Se ha producido un error en la solicitud.",
-          status: "error",
-          position: "top-right",
-          duration: 5000,
-          isClosable: true,
-        });
-      })
-      .finally(() => {
-        // Realizar tareas finales aquí, como limpiar estados o ejecutar acciones después de la solicitud
-      });
-    };
-    return (
-      <Tbody>
-        {itemsToDisplay.map((e) => (
-          <Tr key={e._id}>
-            <Td>
-              <Input
-                variant="filled"
-                defaultValue={e.nombre ? e.nombre : ""}
-                onChange={(event) => handleChangeInput(event, e._id, "nombre")}
-              />
-            </Td>
-            <Td>
-              <Input
-                variant="filled"
-                defaultValue={e.direccion ? e.direccion : ""}
-                onChange={(event) => handleChangeInput(event, e._id, "direccion")}
-              />
-            </Td>
-            <Td>
-              <Input
-                variant="filled"
-                defaultValue={e.direccion2 ? e.direccion2 : ""}
-                onChange={(event) => handleChangeInput(event, e._id, "direccion2")}
-              />
-            </Td>
-            <Td>
-              <Input
-                variant="filled"
-                defaultValue={e.country ? e.country : ""}
-                onChange={(event) => handleChangeInput(event, e._id, "country")}
-              />
-            </Td>
-            <Td>
-              <Input
-                variant="filled"
-                defaultValue={e.taxId ? e.taxId : ""}
-                onChange={(event) => handleChangeInput(event, e._id, "taxId")}
-              />
-            </Td>
-            <Td>
-              <Input
-                variant="filled"
-                defaultValue={e.plantNumber ? e.plantNumber : ""}
-                onChange={(event) => handleChangeInput(event, e._id, "plantNumber")}
-              />
-            </Td>
-            <Td>
-              <Input
-                variant="filled"
-                defaultValue={e.brand ? e.brand : ""}
-                onChange={(event) => handleChangeInput(event, e._id, "brand")}
-              />
-            </Td>
-            <Td>
-              {e.modified ? (
-                <IconButton
-                  colorScheme="blue"
-                  variant="solid"
-                  icon={<FiSave />}
-                  aria-label="save"
-                  onClick={() => handleUpdate(e._id)}
-                />
-              ) : (
-                <IconButton
-                  colorScheme="red"
-                  variant="solid"
-                  icon={<DeleteIcon />}
-                  aria-label="Delete"
-                  onClick={() => handleDelete(e._id)}
-                />
-              )}
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-    );
-  };
   return (
     <>
       {loadData ? (
         <TablePagination
           data={CarteraProveedores}
           params={params}
-          Estructura={Estructura}
+          url="proveedor"
         />
       ) : (
         <Center h="80vh">
--- src/utils/tablePagination.js
+++ src/utils/tablePagination.js
@@ -10,19 +10,26 @@ import {
   InputGroup,
   InputLeftElement,
   Text,
-  Button
+  Button,
+  Td,
+  IconButton,
+  Tbody,
+  useToast,
 } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { FiSave } from "react-icons/fi";
 import { useEffect, useState } from "react";
 import { SearchIcon } from "@chakra-ui/icons";
 import Pagination from "@choc-ui/paginator";
 
-export const TablePagination = ({ data, params, Estructura }) => {
+export const TablePagination = ({ data, params,url }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(10);
   const [searchResults, setSearchResults] = useState([]);
   const [itemsToDisplay, setItemsToDisplay] = useState([]);
   const [searchText, setSearchText] = useState("");
   const itemsPerPage = 6;
+  const toast = useToast();
   const handleSearchChange = (event) => {
     setSearchText(event.target.value);
     setCurrentPage(1);
@@ -51,62 +58,181 @@ export const TablePagination = ({ data, params, Estructura }) => {
     }
   }, [currentPage, searchText]);
   const handleCreate = () => {
-    setItemsToDisplay([...itemsToDisplay.slice(startIndex, endIndex - 1),{_id:Math.random() * 100,state:"nuevo"}])
-  }
+    setItemsToDisplay([
+      ...itemsToDisplay.slice(startIndex, endIndex - 1),
+      { _id: Math.random() * 100, state: "nuevo" },
+    ]);
+  };
+  const token = localStorage.getItem("token");
+  const handleChangeInput = (event, id, parameter) => {
+    const updatedProductos = itemsToDisplay.map((producto) => {
+      if (producto._id === id) {
+        return {
+          ...producto,
+          modified: true,
+          [parameter]: event.target.value,
+        };
+      }
+      return producto;
+    });
+    setItemsToDisplay(updatedProductos);
+  };
+  const handleDelete = (id) => {
+    fetch(`${process.env.API_URL}/${url}/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        toast({
+          title: "Cliente",
+          description: `Se ha borrado correctamente.`,
+          status: "success",
+          position: "top-right",
+          duration: 5000,
+          isClosable: true,
+        });
+      })
+      .finally(() => {
+        const filtered = itemsToDisplay.filter((e) => e._id !== id);
+        setItemsToDisplay(filtered);
+      });
+  };
+  const handleUpdate = (id) => {
+    const buscado = itemsToDisplay.find((element) => element._id === id);
+    fetch(`${process.env.API_URL}/${url}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(buscado),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error en la solicitud");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        toast({
+          title: "Cliente",
+          description: `Se ha guardado correctamente ${data.nombre}.`,
+          status: "success",
+          position: "top-right",
+          duration: 5000,
+          isClosable: true,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        toast({
+          title: "Error",
+          description: "Se ha producido un error en la solicitud.",
+          status: "error",
+          position: "top-right",
+          duration: 5000,
+          isClosable: true,
+        });
+      })
+      .finally(() => {
+        // Realizar tareas finales aquí, como limpiar estados o ejecutar acciones después de la solicitud
+      });
+  };
   return (
-<Box w="100%" boxShadow='2xl' p='6' rounded='md'>
-        <Flex justify="space-between">
-            <InputGroup w="87%">
-              <InputLeftElement pointerEvents="none">
-                <SearchIcon color="gray.300" />
-              </InputLeftElement>
-              <Input
-                type="text"
-                variant="filled"
-                placeholder="Buscar..."
-                value={searchText}
-                onChange={handleSearchChange}
-                borderRadius="full"
-              />
-            </InputGroup>         
-          <Button w="12%" colorScheme="orange" onClick={handleCreate}>Nuevo elemento</Button>
-          </Flex>
-        {itemsToDisplay.length > 0 ? (
-          <TableContainer w="100%">
-            <Table variant="striped" colorScheme="orange">
-              <Thead>
-                <Tr>
-                  {params.map((e, index) => (
-                    <Th w={e == "DESCRIPCION" && "50%"} key={index}>
-                      {e}
-                    </Th>
+    <Box w="100%" boxShadow="2xl" p="6" rounded="md">
+      <Flex justify="space-between">
+        <InputGroup w="87%">
+          <InputLeftElement pointerEvents="none">
+            <SearchIcon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            type="text"
+            variant="filled"
+            placeholder="Buscar..."
+            value={searchText}
+            onChange={handleSearchChange}
+            borderRadius="full"
+          />
+        </InputGroup>
+        <Button w="12%" colorScheme="orange" onClick={handleCreate}>
+          Nuevo elemento
+        </Button>
+      </Flex>
+      {itemsToDisplay.length > 0 ? (
+        <TableContainer w="100%">
+          <Table variant="striped" colorScheme="orange">
+            <Thead>
+              <Tr>
+                {params.map((e, index) => (
+                  <Th w={e.label == "DESCRIPCION" && "50%"} key={index}>
+                    {e.label}
+                  </Th>
+                ))}
+                <Th>ACTIONS</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {itemsToDisplay.map((e) => (
+                <Tr key={e._id}>
+                  {params.map((param, index) => (
+                    <Td key={index}>
+                      <Input
+                        variant="filled"
+                        defaultValue={e[param.param] ? e[param.param] : ""}
+                        onChange={(event) =>
+                          handleChangeInput(event, e._id, param.param)
+                        }
+                      />
+                    </Td>
                   ))}
+                  <Td>
+                    {e.modified ? (
+                      <IconButton
+                        colorScheme="blue"
+                        variant="solid"
+                        icon={<FiSave />}
+                        aria-label="save"
+                        onClick={() => handleUpdate(e._id)}
+                      />
+                    ) : (
+                      <IconButton
+                        colorScheme="red"
+                        variant="solid"
+                        icon={<DeleteIcon />}
+                        aria-label="Delete"
+                        onClick={() => handleDelete(e._id)}
+                      />
+                    )}
+                  </Td>
                 </Tr>
-              </Thead>
-              {Estructura(itemsToDisplay, setItemsToDisplay)}
-            </Table>
-          </TableContainer>
-        ) : (
-          <Text mt={10}>No se han encontrado Resultados.</Text>
-        )}
-        {itemsToDisplay.length > 0 && (
-          <Flex w="full" p={50} alignItems="center" justifyContent="center">
-            <Pagination
-              current={currentPage}
-              onChange={(page) => setCurrentPage(page)}
-              total={totalPages}
-              paginationProps={{
-                display: "flex",
-              }}
-              activeStyles={{
-                bg: "orange.400",
-              }}
-              hoverStyles={{
-                bg: "cyan.300",
-              }}
-            />
-          </Flex>
-        )}
-      </Box>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <Text mt={10}>No se han encontrado Resultados.</Text>
+      )}
+      {itemsToDisplay.length > 0 && (
+        <Flex w="full" p={50} alignItems="center" justifyContent="center">
+          <Pagination
+            current={currentPage}
+            onChange={(page) => setCurrentPage(page)}
+            total={totalPages}
+            paginationProps={{
+              display: "flex",
+            }}
+            activeStyles={{
+              bg: "orange.400",
+            }}
+            hoverStyles={{
+              bg: "cyan.300",
+            }}
+          />
+        </Flex>
+      )}
+    </Box>
   );
 };
