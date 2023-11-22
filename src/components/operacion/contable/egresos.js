dac4308842919614201ee63de2dae8d26a243ce3
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2023-11-22 16:02:13 -0300

    fixes

--- src/components/operacion/contable/egresos.js
+++ src/components/operacion/contable/egresos.js
@@ -1,4 +1,14 @@
-import { Grid, GridItem, VStack, Text, Center } from "@chakra-ui/react";
+import {
+  Grid,
+  GridItem,
+  VStack,
+  Text,
+  Center,
+  Heading,
+  Badge,
+  Divider,
+  Box,
+} from "@chakra-ui/react";
 import InputPersonalizado from "@/utils/inputPersonalizado";
 import { ComisionesBox } from "@/utils/comicionesBox";
 import { useStore } from "@/store/operation";
@@ -17,25 +27,58 @@ export const EgresosForm = ({ operation, setOperation }) => {
     <VStack w="full" spacing="3">
       <Grid w="100%" templateColumns="repeat(2, 1fr)" gap={2}>
         <GridItem w="full">
-          <VStack w="full" spacing="3">
-            <Center>
-              <Text as="b">Egresos</Text>
-            </Center>
-            <InputPersonalizado
-              label="Nro Factura Proveedor Frigorifico"
-              onChange={(e) => handleChange(e, "nroFacturaProveedorFrigo")}
-            />
-            <InputPersonalizado
-              label="Monto Total Factura"
-              value={convertirAMoneda(operation.contableFinanciera.fields.totalFacturaCompra)}
-              onChange={(e) => handleChange(e, "MontoTotalFactura")}
-            />
-            <InputPersonalizado label="Fecha de Pago Anticipo" />
-            <InputPersonalizado label="Monto Pagado Anticipo" />
-            <ComisionesBox text="COMISIONES USD 1500" />
-            <InputPersonalizado label="Fecha de Pago Balance / Total" />
-            <InputPersonalizado label="Monto Pagado Balance" />
-            <ComisionesBox text="COMISIONES USD 1500" />
+          <VStack spacing="3">
+            <Box
+              w="full"
+              border="2px"
+              borderColor="blue.100"
+              borderStyle="dashed"
+              borderRadius="lg"
+              p={1}
+            >
+              <VStack spacing="3">
+                <Badge
+                  colorScheme="blue"
+                  fontSize="lg"
+                  w="full"
+                  textAlign="center"
+                >
+                  Purchase
+                </Badge>
+                <InputPersonalizado
+                  label="Nro Factura Proveedor Frigorifico"
+                  onChange={(e) => handleChange(e, "nroFacturaProveedorFrigo")}
+                />
+                <InputPersonalizado
+                  label="Monto Total Factura"
+                  value={convertirAMoneda(
+                    operation.contableFinanciera.fields.totalFacturaCompra
+                  )}
+                  onChange={(e) => handleChange(e, "MontoTotalFactura")}
+                />
+                <InputPersonalizado
+                  label="Fecha de Pago Anticipo"
+                  type="date"
+                />
+                <InputPersonalizado label="Monto Pagado Anticipo" />
+                <ComisionesBox
+                  text="COMISIONES"
+                  monto={200}
+                  bank={operation.comercial.fields.empresa.bank}
+                />
+                <Divider />
+                <InputPersonalizado
+                  label="Fecha de Pago Balance / Total"
+                  type="date"
+                />
+                <InputPersonalizado label="Monto Pagado Balance" />
+                <ComisionesBox
+                  text="COMISIONES"
+                  monto={200}
+                  bank={operation.comercial.fields.empresa.bank}
+                />
+              </VStack>
+            </Box>
             {operation.comercial.fields.operationType ==
               "Trading + Marketing" && (
               <VStack
@@ -48,10 +91,26 @@ export const EgresosForm = ({ operation, setOperation }) => {
                 borderRadius="lg"
                 p={1}
               >
+                <Badge
+                  colorScheme="blue"
+                  fontSize="lg"
+                  w="full"
+                  textAlign="center"
+                >
+                  Marketing
+                </Badge>
                 <InputPersonalizado label="Nro Factura Proveedor Marketing" />
-                <InputPersonalizado label="Monto Factura" value={convertirAMoneda(operation.comercial.fields.comisionMarketing)}/>
-                <InputPersonalizado label="Fecha de pago" />
-                <ComisionesBox text="COMISIONES USD 1500" />
+                <InputPersonalizado
+                  label="Monto Factura"
+                  value={convertirAMoneda(
+                    operation.comercial.fields.comisionMarketing
+                  )}
+                />
+                <InputPersonalizado label="Fecha de pago" type="date" />
+                <ComisionesBox
+                  text="COMISIONES"
+                  bank={operation.comercial.fields.empresa.bank}
+                />
               </VStack>
             )}
             {(operation.comercial.fields.deliveryTermsSale == "CIF" ||
@@ -66,10 +125,21 @@ export const EgresosForm = ({ operation, setOperation }) => {
                 borderRadius="lg"
                 p={1}
               >
+                <Badge
+                  colorScheme="blue"
+                  fontSize="lg"
+                  w="full"
+                  textAlign="center"
+                >
+                  Freight
+                </Badge>
                 <InputPersonalizado label="Nro Factura Proveedor Flete internacional" />
                 <InputPersonalizado label="Monto Factura" />
                 <InputPersonalizado label="Fecha de pago" />
-                <ComisionesBox text="COMISIONES USD 1500" />
+                <ComisionesBox
+                  text="COMISIONES"
+                  bank={operation.comercial.fields.empresa.bank}
+                />
               </VStack>
             )}
             {operation.comercial.fields.deliveryTermsSale == "CIF" && (
@@ -83,45 +153,120 @@ export const EgresosForm = ({ operation, setOperation }) => {
                 borderRadius="lg"
                 p={1}
               >
+                <Badge
+                  colorScheme="blue"
+                  fontSize="lg"
+                  w="full"
+                  textAlign="center"
+                >
+                  Insurance
+                </Badge>
                 <InputPersonalizado label="Nro Factura Proveedor Seguro internacional" />
                 <InputPersonalizado label="Monto Factura" />
-                <InputPersonalizado label="Fecha de pago" />
-                <ComisionesBox text="COMISIONES USD 1500" />
+                <InputPersonalizado label="Fecha de pago" type="date" />
+                <ComisionesBox
+                  text="COMISIONES"
+                  bank={operation.comercial.fields.empresa.bank}
+                />
               </VStack>
             )}
           </VStack>
         </GridItem>
         <GridItem w="full">
-          <VStack w="full" spacing="3">
-            <Center>
-              <Text as="b">Ingresos</Text>
-            </Center>
+          <VStack w="full"
+            spacing="3">
+
+          
+          <VStack
+            w="full"
+            spacing="3"
+            border="2px"
+            borderColor="green.100"
+            borderStyle="dashed"
+            borderRadius="lg"
+            p={1}
+          >
+            <Badge colorScheme="green" fontSize="lg" w="full" textAlign="center">
+              Sell
+            </Badge>
             <InputPersonalizado label="Nro Factura" />
             <InputPersonalizado
               label="Monto total Factura"
-              value={convertirAMoneda(operation.contableFinanciera.fields.totalFacturaVenta)}
+              value={convertirAMoneda(
+                operation.contableFinanciera.fields.totalFacturaVenta
+              )}
             />
-            <InputPersonalizado label="Fecha de cobro Anticipo" />
+            <InputPersonalizado label="Fecha de cobro Anticipo" type="date" />
             <InputPersonalizado label="Monto Cobrado Anticipo" />
             <Grid w="100%" templateColumns="repeat(2, 1fr)" gap={2}>
               <GridItem>
-                <ComisionesBox text="COMISIONES USD 1500" />
+                <ComisionesBox
+                  text="COMISIONES"
+                  bank={operation.comercial.fields.empresa.bank}
+                />
               </GridItem>
               <GridItem>
-                <ComisionesBox text="CHARGES USD 1200" colorScheme="green" />
+                <ComisionesBox
+                  text="CHARGES"
+                  colorScheme="green"
+                  bank={operation.comercial.fields.empresa.bank}
+                />
               </GridItem>
             </Grid>
-            <InputPersonalizado label="Fecha de cobro Balance" />
+            <Divider/>
+            <InputPersonalizado label="Fecha de cobro Balance" type="date" />
             <InputPersonalizado label="Monto cobrado Balance" />
             <Grid w="100%" templateColumns="repeat(2, 1fr)" gap={2}>
               <GridItem>
-                <ComisionesBox text="COMISIONES USD 1500" />
+                <ComisionesBox
+                  text="COMISIONES"
+                  bank={operation.comercial.fields.empresa.bank}
+                />
               </GridItem>
               <GridItem>
-                <ComisionesBox text="CHARGES USD 1200" colorScheme="green" />
+                <ComisionesBox
+                  text="CHARGES"
+                  colorScheme="green"
+                  bank={operation.comercial.fields.empresa.bank}
+                />
               </GridItem>
             </Grid>
           </VStack>
+          {operation.comercial.fields.operationType ==
+              "Broker" && (
+              <VStack
+                spacing="3"
+                w="full"
+                shadow="lg"
+                border="2px"
+                borderColor="blue.100"
+                borderStyle="dashed"
+                borderRadius="lg"
+                p={1}
+              >
+                <Badge
+                  colorScheme="green"
+                  fontSize="lg"
+                  w="full"
+                  textAlign="center"
+                >
+                  Brokerage
+                </Badge>
+                <InputPersonalizado label="Nro Factura" />
+                <InputPersonalizado
+                  label="Monto Factura"
+                  value={convertirAMoneda(
+                    operation.comercial.fields.comisionPurchase || operation.comercial.fields.comisionSale
+                  )}
+                />
+                <InputPersonalizado label="Fecha de Cobro" type="date" />
+                <ComisionesBox
+                  text="COMISIONES"
+                  bank={operation.comercial.fields.empresa.bank}
+                />
+              </VStack>
+            )}
+            </VStack>
         </GridItem>
       </Grid>
     </VStack>
--- src/utils/comicionesBox.js
+++ src/utils/comicionesBox.js
@@ -1,10 +1,16 @@
-import { Badge } from "@chakra-ui/react";
+import { Text, Tooltip } from "@chakra-ui/react";
+import InputPersonalizado from "./inputPersonalizado";
 
-export const ComisionesBox = ({ text,fontSize,colorScheme }) => {
+export const ComisionesBox = ({ text,monto,bank,fontSize,colorScheme }) => {
+    const porcentaje = text === "COMISIONES" ? (bank?.porcentaje * monto || 0) : (bank?.porcentajeCobranza * monto || 0);
+    const minimo = text === "COMISIONES" ? (bank?.minimo || 0) : (bank?.minimoCobranza || 0);
+    const fijo = text === "COMISIONES" ? (bank?.fijo || 0) : (bank?.fijoCobranza || 0);
+    const calc = monto > minimo ? monto + porcentaje + fijo : "No supera el mínimo";
+    
   return (
-        <Badge colorScheme={colorScheme || "telegram"} fontSize={fontSize || "md"} w="full" textAlign="center" borderRadius="lg" p={1}>
-        {text}
-        </Badge>
+    <Tooltip label={calc ? `comision = ${monto} + ${porcentaje} + ${fijo}` : ""} aria-label={text}>
+        <InputPersonalizado label={text} defaultValue={`${monto ? calc : "0 USD"}`} />
+    </Tooltip>
   );
 }
 
