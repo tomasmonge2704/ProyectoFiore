d9050dd4c643e7c46489fecad9bb8f347ff4fd3d
tomasmonge2704 <tomas.monge2704@gmail.com>
2023-11-28 10:32:57 -0300

    fixed state selector

--- src/utils/stateSelector.js
+++ src/utils/stateSelector.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Popover,
   PopoverTrigger,
@@ -25,8 +25,10 @@ export const StateSelector = ({ selected, refNumber }) => {
     "Canceled":"red"
   };
 
-  const initialFocusRef = React.useRef();
   const [selectedValue, setSelectedValue] = useState(selected);
+  useEffect(() => {
+    setSelectedValue(selected)
+  },[selected])
   const [isLoading, setIsLoading] = useState(false);
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const color = useColorModeValue('black','white');
@@ -34,6 +36,7 @@ export const StateSelector = ({ selected, refNumber }) => {
     setIsLoading(true);
     const token = localStorage.getItem("token");
     const selectedOption = event.target.value;
+    
     fetch(`${process.env.API_URL}/operation/by-ref/${refNumber}/status`, {
       method: "PUT",
       headers: {
@@ -41,28 +44,29 @@ export const StateSelector = ({ selected, refNumber }) => {
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({ status: selectedOption }),
-    }).then((response) => {
-        if (!response.ok) {
-          throw new Error("La solicitud no fue exitosa");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setIsLoading(false);
-        setSelectedValue(data);
-        setIsPopoverOpen(false); // Cerrar el Popover cuando la petición sea exitosa
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        console.log(err)
-      });
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("La solicitud no fue exitosa");
+      }
+      return response.json();
+    })
+    .then(() => {
+      setIsLoading(false);
+      setSelectedValue(selectedOption); // Update with the selected option
+      setIsPopoverOpen(false); // Close the Popover when the request is successful
+    })
+    .catch((err) => {
+      setIsLoading(false);
+      console.log(err);
+    });
   };
+  
 
   return (
     <Popover
       isOpen={isPopoverOpen}
       onOpen={() => setIsPopoverOpen(true)} // Abre el Popover cuando sea necesario
-      initialFocusRef={initialFocusRef}
       placement="bottom"
       closeOnBlur={false}
     >
@@ -73,6 +77,7 @@ export const StateSelector = ({ selected, refNumber }) => {
       </PopoverTrigger>
       <PopoverContent bg="transparent" backdropFilter="blur(20px) saturate(1.5)" border="1px" borderColor="Background" shadow="md" >
         <PopoverHeader pt={4} fontWeight="bold" border="0">
+          {refNumber}
         </PopoverHeader>
         <PopoverArrow bg="transparent"/>
         <PopoverCloseButton onClick={() => setIsPopoverOpen(false)}/>
