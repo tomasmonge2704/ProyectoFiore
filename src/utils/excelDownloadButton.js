76ba29bf618f57e070ca45c0f1e91e91833974da
Tomas Monge <tomasmonge@MacBook-Pro.fibertel.com.ar>
2024-08-02 03:37:37 -0300

    fix

--- src/utils/excelDownloadButton.js
+++ src/utils/excelDownloadButton.js
@@ -3,19 +3,39 @@ import { FiDownload } from "react-icons/fi";
 import React from "react";
 import * as ExcelJS from "exceljs";
 import useFetch from "@/hooks/useFetch";
+
+const convertDateStringsToDates = (row) => {
+  // Aquí puedes especificar los índices de las columnas que contienen fechas.
+  const dateIndices = [1, 24, 25, 28, 29, 34, 38, 40, 44, 49];
+  return row.map((cell, index) => {
+    if (dateIndices.includes(index) && typeof cell === 'string') {
+      // Intenta convertir la cadena de fecha en un objeto Date.
+      const date = new Date(cell);
+      // Verifica si la conversión fue exitosa.
+      if (!isNaN(date.getTime())) {
+        return date;
+      }
+    }
+    return cell;
+  });
+};
+
 const generateExcelFile = async (data) => {
   const workbook = new ExcelJS.Workbook();
   const worksheet = workbook.addWorksheet("Sheet 1");
 
-  // Agregar datos al archivo Excel
+  // Agregar datos al archivo Excel, convirtiendo fechas a objetos Date
   data.forEach((row) => {
-    worksheet.addRow(row);
+    const convertedRow = convertDateStringsToDates(row);
+    worksheet.addRow(convertedRow);
   });
+
   // Establecer el formato de fecha para las columnas que contienen fechas
   const dateColumns = ["B","Y","Z","AC","AD", "AI", "AM", "AO", "AS", "AX"];
   dateColumns.forEach((column) => {
     worksheet.getColumn(column).numFmt = "dd/mm/yyyy";
   });
+
   // Crear un blob que contiene el archivo Excel
   const blob = await workbook.xlsx.writeBuffer();
 
@@ -39,7 +59,7 @@ export const ExcelIconButton = () => {
   return (
     <IconButton
       icon={<FiDownload />}
-      isLoading={dataExcel ? false : true}
+      isLoading={!dataExcel}
       onClick={() => generateExcelFile(dataExcel)}
     />
   );
